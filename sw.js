a3e384cd48bf5e499e2d52f7a98d56594a929922
Radim Pernický <radimpernicky@Radim--Mac-mini.local>
2026-04-22 22:48:27 +0200

    Initial commit: Bezira AI Infographic Generator

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'svg-infografika-v2';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json',
+    './icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+                // Pokud síť funguje, uložíme novou verzi do cache
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            })
+            .catch(() => {
+                // Když jsme offline, vrátíme z cache
+                return caches.match(event.request);
+            })
+    );
+});
